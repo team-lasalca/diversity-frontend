897d1c69f91953144fa0e27c48ed79356842ce89
QwertygidQ <Qwertygid@gmail.com>
2019-09-06 20:35:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# diversity-hack-frontend
+
+A frontend for the Moscow Diversity Hack web application.  
+Made by *Team Lasalca*.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>Place a hackathon web-app here...</h1>
+  );
+}
+
+export default App;
